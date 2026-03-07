f8986bf32fdc4d7625c6761b8a7e269941f5e2ca
Matias Cano <matias.cano@dataart.com>
2026-03-07 20:59:44 -0300

    #7 Integrar teminologia de modulacion

--- DomainLogic/Services/AmbienteConfig.cs
+++ DomainLogic/Services/AmbienteConfig.cs
@@ -11,30 +11,33 @@ namespace DomainLogic.Services
         public static Designacion CrearAmbiente(ILogger logger)
         {
             logger.LogInformation("═══ Configurando ambiente ═══\n");
-            var frecuenciaBase = 100000.0; //Frecuencia base para el espacio, se asume que es la más alta para que las demás interactuen con ella.  
-            var frecuenciaExtrema = frecuenciaBase * 2;
-            var frecuenciaAlta = frecuenciaBase/100;
-            var frecuenciaMediaAlta = frecuenciaBase/1000;
-            var frecuenciaMediaBaja = frecuenciaBase/5000;
+            var frecuenciaBase = Math.PI / 2 * 1000000; //Frecuencia base para el espacio, se asume que es la más alta para que las demás interactuen con ella.            
+            var frecuenciaAlta = frecuenciaBase/10;
+            var frecuenciaMedia = frecuenciaBase/100;
+            var frecuenciaMediaBaja = frecuenciaBase/1000;
             var frecuenciaBaja = frecuenciaBase/10000;
+                    
+            var tiempo = Designacion.Imaginar("Parecido", "Tiempo", "Parecer", 0, Math.PI);
+            var espacio = Designacion.Imaginar("Estado", "Espacio", "Estar", frecuenciaBase, 0);
+            var yo = Designacion.Designar(espacio.Nombre, tiempo.Apariencia, "Yo");
 
-            var espacio = Designacion.Imaginar("Vacío", "Espacio", "Estar", 0, frecuenciaBase);
-            var tiempo = Designacion.Imaginar("Puro", "Tiempo", "Ser", 2 * Math.PI, 0);   
-            var tierraPura = Designacion.Designar(espacio.Nombre, tiempo.Apariencia, "TierraPura", frecuenciaAlta, 0);
-            var aguaPura = Designacion.Designar(tierraPura.Nombre, tierraPura.Apariencia, "AguaPura", frecuenciaBase);
-            var airePuro = Designacion.Designar(aguaPura.Nombre, aguaPura.Apariencia, "AirePuro", frecuenciaMediaBaja);
-            var fuegoPuro = Designacion.Designar(airePuro.Nombre, airePuro.Apariencia, "FuegoPuro", frecuenciaBaja);
+            var tierraPura = Designacion.Imaginar("Pura", "Tierra", "Permanecer", frecuenciaAlta, 0);
+            var aguaPura = Designacion.Imaginar("Pura", "Agua", "Fluir", frecuenciaMediaBaja, Math.PI / 2);            
+            var airePuro = Designacion.Imaginar("Puro", "Aire", "Mover", frecuenciaMedia, Math.PI);
+            var fuegoPuro = Designacion.Imaginar("Caliente", "Fuego", "Ser", frecuenciaBaja, 3 * Math.PI / 2);
+            
+            var yoPuro = Designacion.Imaginar("Frío", "Yo", "Ser", 3 * Math.PI / 2, frecuenciaBase);
+            var espacioTiempo = Designacion.Designar(yoPuro.Nombre, yo.Apariencia, "Espacio-Tiempo");
 
-            var aguaFluida = Designacion.Designar(aguaPura.Nombre, fuegoPuro.Apariencia, "Agua", frecuenciaAlta, 3 * Math.PI / 2);
-            var tierraSolida = Designacion.Designar(tierraPura.Nombre, aguaFluida.Apariencia, "Tierra", frecuenciaMediaBaja);            
-            var aireDisperso = Designacion.Designar(aguaPura.Nombre, tierraSolida.Apariencia, "Aire", frecuenciaMediaAlta);
-            var fuegoCaliente = Designacion.Designar(fuegoPuro.Nombre, aireDisperso.Apariencia, "Fuego", frecuenciaBaja, 0);
+            var agua = Designacion.Designar(aguaPura.Nombre, espacioTiempo.Apariencia, "Agua", null, -Math.PI);
+            var tierra = Designacion.Designar(tierraPura.Nombre, agua.Apariencia, "Tierra");
+            var aire = Designacion.Designar(airePuro.Nombre, tierra.Apariencia, "Aire");
+            var fuego = Designacion.Designar(fuegoPuro.Nombre, aire.Apariencia, "Fuego", 0.18);
 
-            var yo = Designacion.Designar(fuegoCaliente.Nombre, fuegoCaliente.Apariencia, "Yo", frecuenciaExtrema, Math.PI);
-            logger.LogInformation(yo.ToString());
+            logger.LogInformation(fuego.ToString());
             logger.LogInformation("═══ Ambiente configurado ═══\n");
-            PilotosOfdmFrame = new List<Designacion> { tierraPura, aguaPura, airePuro, fuegoPuro };
-            SubPilotosOfdmFrame = new List<Designacion> { aguaFluida, tierraSolida, aireDisperso, fuegoCaliente };
+            PilotosOfdmFrame = new List<Designacion> { agua, tierra, aire, fuego };
+            SubPilotosOfdmFrame = new List<Designacion> { espacioTiempo, yoPuro, tierraPura, aguaPura, airePuro, fuegoPuro };
             return fuegoPuro;
         }
     }
