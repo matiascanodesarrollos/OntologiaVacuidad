62f3f48eacda0429280637be52d021ad2bb13a7e
Matias Cano <matias.cano@dataart.com>
2026-04-17 15:55:44 -0300

    Fix

--- DomainLogic.Tests/AmbienteConfigTests.cs
+++ DomainLogic.Tests/AmbienteConfigTests.cs
@@ -13,9 +13,9 @@ public class AmbienteConfigTests
         Assert.Equal(2.0943951024, nombres[1].Fase, 10);
         Assert.Equal(4.1887902048, nombres[2].Fase, 10);
         Assert.Equal("pensar humano", nombres[2].Texto);
-        Assert.Equal(2d, nombres[0].Esencia.Amplitud(0d), 10);
-        Assert.Equal(0.3660254038d, nombres[1].Esencia.Amplitud(0d), 10);
-        Assert.Equal(double.MaxValue, nombres[2].Esencia.Amplitud(0d));
+        Assert.Equal((2d, 0d), nombres[0].Esencia.Amplitud(0d));
+        Assert.Equal((-0.49999999999999978, 1.7320508075688774), nombres[1].Esencia.Amplitud(0d));
+        Assert.Equal((1.7976931348623157E+308, 1.7976931348623157E+308), nombres[2].Esencia.Amplitud(0d));
         Assert.Equal(1d, ambiente.VelocidadGrupo(nombres[0]), 10);
     }
 }
